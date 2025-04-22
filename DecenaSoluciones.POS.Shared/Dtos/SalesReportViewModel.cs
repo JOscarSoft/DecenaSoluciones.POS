e4e8bf0a16bdc50e8455484fa5a39989dddc9f56
JOscarSoft <jordy.dcna@gmail.com>
2025-04-22 15:00:02 -0400

    Adding amounts to excel

--- DecenaSoluciones.POS.Shared/Dtos/SalesReportViewModel.cs
+++ DecenaSoluciones.POS.Shared/Dtos/SalesReportViewModel.cs
@@ -7,6 +7,12 @@ namespace DecenaSoluciones.POS.Shared.Dtos
         public string? Code { get; set; }
         public string? CustomerName { get; set; }
         public int ProductsQuantity { get; set; }
+        public decimal? PayedAmount { get; set; }
+        public decimal Devolution { get
+            {
+                return (PayedAmount ?? 0m) - (Total ?? 0m);
+            }
+        }
         public decimal? Total { get; set; }
         public string? UserName { get; set; }
         public DateTime CreationDate { get; set; }
